a0cba7996758de4bdd8355904e9366b49b7dce42
dbk1985 <376958755@qq.com>
2019-03-15 13:44:57 +0800

    bind angualrjs ng-model

--- dist/angular-bootstrap-duallistbox.js
+++ dist/angular-bootstrap-duallistbox.js
@@ -1,6 +1,6 @@
 /**
  * angular-bootstrap-duallistbox
- * @version v0.1.0 - 2019-03-14
+ * @version v0.1.0 - 2019-03-15
  * @author Francesco Pontillo (francescopontillo@gmail.com)
  * @link https://github.com/dbk1985/angular-bootstrap-duallistbox
  * @license Apache License 2.0
@@ -17,8 +17,12 @@ angular.module('frapontillo.bootstrap-duallistbox').directive('bsDuallistbox', [
     return {
       restrict: 'A',
       require: 'ngModel',
-      link: function link(scope, element, attrs) {
+      link: function link(scope, $element, attrs) {
         element = $(element);
+        var element = $($element);
+        element.bind('change', function () {
+          $element.triggerHandler('change');
+        });
         //000011111111110000000000022222222220000000000000000000003333333333000000000000004444444444444440000000005555555555555550000000666666666666666000000000000000777777777700000000000000000008888888888
         var NG_OPTIONS_REGEXP = /^\s*([\s\S]+?)(?:\s+as\s+([\s\S]+?))?(?:\s+group\s+by\s+([\s\S]+?))?\s+for\s+(?:([\$\w][\$\w]*)|(?:\(\s*([\$\w][\$\w]*)\s*,\s*([\$\w][\$\w]*)\s*\)))\s+in\s+([\s\S]+?)(?:\s+track\s+by\s+([\s\S]+?))?$/;
         // The select collection
--- src/directives/bsDuallistbox.js
+++ src/directives/bsDuallistbox.js
@@ -5,8 +5,12 @@ angular.module('frapontillo.bootstrap-duallistbox')
     return {
       restrict: 'A',
       require: 'ngModel',
-      link: function link(scope, element, attrs) {
+      link: function link(scope, $element, attrs) {
         element = $(element);
+        var element = $($element);
+        element.bind('change',function () {
+          $element.triggerHandler('change');
+        });
                                //000011111111110000000000022222222220000000000000000000003333333333000000000000004444444444444440000000005555555555555550000000666666666666666000000000000000777777777700000000000000000008888888888
         var NG_OPTIONS_REGEXP = /^\s*([\s\S]+?)(?:\s+as\s+([\s\S]+?))?(?:\s+group\s+by\s+([\s\S]+?))?\s+for\s+(?:([\$\w][\$\w]*)|(?:\(\s*([\$\w][\$\w]*)\s*,\s*([\$\w][\$\w]*)\s*\)))\s+in\s+([\s\S]+?)(?:\s+track\s+by\s+([\s\S]+?))?$/;
 
